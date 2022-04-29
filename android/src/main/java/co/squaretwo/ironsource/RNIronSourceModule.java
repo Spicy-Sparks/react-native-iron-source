43f5c225095647e65fa44bc51eb68b671e985be3
Marco Fontana <fontana.marco@hotmail.com>
2022-04-29 19:18:12 +0200

    Update RNIronSourceModule.java

--- android/src/main/java/co/squaretwo/ironsource/RNIronSourceModule.java
+++ android/src/main/java/co/squaretwo/ironsource/RNIronSourceModule.java
@@ -4,14 +4,24 @@ import android.app.Activity;
 import android.os.Handler;
 import android.os.Looper;
 
+import androidx.annotation.Nullable;
+
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.ironsource.mediationsdk.IronSource;
+import com.ironsource.mediationsdk.impressionData.ImpressionData;
+import com.ironsource.mediationsdk.impressionData.ImpressionDataListener;
 import com.ironsource.mediationsdk.integration.IntegrationHelper;
+import com.ironsource.mediationsdk.logger.IronSourceError;
+import com.ironsource.mediationsdk.sdk.InterstitialListener;
+
+import org.json.JSONException;
 
 public class RNIronSourceModule extends ReactContextBaseJavaModule {
     private static final String TAG = "RNIronSource";
@@ -61,6 +71,8 @@ public class RNIronSourceModule extends ReactContextBaseJavaModule {
                 final boolean validateIntegration = options.getBoolean("validateIntegration");
 
                 IronSource.setUserId(userId);
+                IronSource.addImpressionDataListener(generateImpressionDataListener());
+
                 IronSource.init(activity, appId);
                 if (activity != null && validateIntegration) {
                     IntegrationHelper.validateIntegration(activity);
@@ -72,8 +84,22 @@ public class RNIronSourceModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
+    public void addImpressionDataDelegate() {
+        IronSource.addImpressionDataListener(generateImpressionDataListener());
+    }
+
+    @ReactMethod
+    public void setUserId(final String userId) {
+        IronSource.setUserId(userId);
+    }
+
+    @ReactMethod
     public void setConsent(boolean consent) {
         IronSource.setConsent(consent);
+    }
+
+    private void sendEvent(String eventName, @Nullable WritableMap params) {
+        getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
     }
 
     @ReactMethod
@@ -84,5 +110,22 @@ public class RNIronSourceModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void removeListeners(double count) {
       // Keep: Required for RN built in Event Emitter Calls.
+    }
+
+    private ImpressionDataListener generateImpressionDataListener(){
+        return new ImpressionDataListener() {
+            @Override
+            public void onImpressionSuccess (ImpressionData impressionData){
+                WritableMap data = Arguments.createMap();
+                data.putDouble("revenue", impressionData.getRevenue());
+                data.putString("ad_network", impressionData.getAdNetwork());
+                try {
+                    data.putMap("all_data", Utility.convertJsonToMap(impressionData.getAllData()));
+                } catch (JSONException e) {
+
+                }
+                sendEvent("impressionDataDidSucceed", data);
+            }
+        };
     }
 }
